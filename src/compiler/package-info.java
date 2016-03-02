4fa9b579bac2bb7a8f08795f4931f533d5af7c55
Toni Kocjan <kocjanturk@gmail.com>
2016-03-02 09:59:31 +0100

    Creation of the PINS Compiler project. This project is part of 'Prevajalniki in Navidezni stroji' subject

---
+++ src/compiler/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Prevajalnik za programski jezik PREV.
+ * <p>
+ * Celotna izvorna koda prevajalnika za programski jezik PINS je zbrana v paketu {@link compiler} in njegovih podpaketih.
+ * 
+ * @author sliva
+ */
+package compiler;
